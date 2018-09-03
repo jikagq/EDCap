25ad4a59a576f8cf7b8b5c6a04dad2552a21edca
jikagq <theo-nec@hotmail.fr>
2018-09-03 12:02:14 +0200

    ajout du bouton

--- app/src/main/java/com/example/pain/edcap/MainActivity.java
+++ app/src/main/java/com/example/pain/edcap/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.pain.edcap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button calc;
 
-    Number latdep, longdep, latar, longar;
+    EditText latdep, longdep, latar, longar;
 
     TextView res;
 
@@ -20,12 +24,28 @@ public class MainActivity extends AppCompatActivity {
 
         calc = (Button)findViewById(R.id.calc);
 
-        latdep = (Number)findViewById(R.id.latdep);
-        longdep =
-        latar =
-        longar =
+        latdep = (EditText)findViewById(R.id.latdep);
+        longdep = (EditText)findViewById(R.id.longdep);
+        latar = (EditText)findViewById(R.id.latar);
+        longar = (EditText)findViewById(R.id.longar);
 
-        res = (TextView)findViewById(R.id.res)        
+        res = (TextView)findViewById(R.id.res) ;
+
+        Log.d("INFO","Elle est ou la balayette ?");
+
+        calc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("INFO","Dans ton cul");
+                Toast.makeText(MainActivity.this, "The BT device is OFF!", Toast.LENGTH_SHORT).show();
+                //mettre les actions de calculs ici
+                //vérifier si y a des valeurs nulles ou 0
+
+
+
+
+            }
+        });
 
 
     }

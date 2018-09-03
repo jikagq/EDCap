0e8548fce69093eed06c590d953c5ccc5a349be1
jikagq <theo-nec@hotmail.fr>
2018-09-03 11:35:26 +0200

    1er commit

---
+++ app/src/main/java/com/example/pain/edcap/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.pain.edcap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button calc;
+
+    Number latdep, longdep, latar, longar;
+
+    TextView res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calc = (Button)findViewById(R.id.calc);
+
+        latdep = (Number)findViewById(R.id.latdep);
+        longdep =
+        latar =
+        longar =
+
+        res = (TextView)findViewById(R.id.res)        
+
+
+    }
+}
